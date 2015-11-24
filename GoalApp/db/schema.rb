2418d799d643f1fcc98ed9a683f954f33c5a7a6c
App Academy Student <aellison1558@gmail.com>
2015-11-23 16:39:47 -0800

    Build UserComment Model

--- GoalApp/db/schema.rb
+++ GoalApp/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151123235359) do
+ActiveRecord::Schema.define(version: 20151124001445) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id",    null: false
@@ -31,6 +31,17 @@ ActiveRecord::Schema.define(version: 20151123235359) do
   end
 
   add_index "goals", ["user_id"], name: "index_goals_on_user_id"
+
+  create_table "user_comments", force: :cascade do |t|
+    t.string   "body",       null: false
+    t.integer  "author_id",  null: false
+    t.integer  "user_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "user_comments", ["author_id"], name: "index_user_comments_on_author_id"
+  add_index "user_comments", ["user_id"], name: "index_user_comments_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "username",        null: false
---
+++ GoalApp/spec/factories/user_comments.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user_comment do
+    body ""
+author_id 1
+user_id 1
+  end
+
+end
